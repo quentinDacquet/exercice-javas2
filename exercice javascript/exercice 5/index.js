f6e7f56361d6381ebc0911fc5cfa54a0199f50ad
quentinDacquet <82867473+quentinDacquet@users.noreply.github.com>
2021-05-18 23:57:44 +0200

    Add files via upload

---
+++ exercice javascript/exercice 5/index.js
@@ -0,0 +1,15 @@
+document.write("<table border=5>");
+document.write("<tr><td>","</td>");
+for(var i=1;i<10;i++)
+document.write("<td>",i,"</td>");
+
+for(var i=1;i<10;i++){
+    document.write("<tr>");
+    document.write("<td>",i,"</td>");
+        for(var j=1;j<10;j++)
+        {	
+            document.write("<td>",i*j,"</td>");
+        }
+    document.write("</tr>");
+    }
+document.write("</table>");
